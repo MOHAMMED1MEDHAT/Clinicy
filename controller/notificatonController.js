88444a2649e2c1aeed049972e3bc2af3e681ec91
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-04-30 02:24:04 +0300

    Update Notification response structure 0.2

--- controller/notificatonController.js
+++ controller/notificatonController.js
@@ -51,10 +51,6 @@ const getAllPatientNotification=async(req,res)=>{
         .exec();
 
         const notification=await Notification.find({appointmentId:appointment._id}).exec();
-        console.log(customResponse)
-        if(!notification.length==0){
-            return res.status(204).json({message:"no notification"});
-        }
         // setting customResponse values 
         customResponse.clinicName=appointment.clinick.clinicName;
         customResponse.doctorName=appointment.clinick.doctor.name;
@@ -63,6 +59,10 @@ const getAllPatientNotification=async(req,res)=>{
         customResponse.bookingTime=appointment.bookingTime;
         customResponse.typeOfNotification=notification.typeOfNotification;
         
+        console.log(customResponse)
+        if(!notification.length==0){
+            return res.status(204).json({message:"no notification"});
+        }
         res.status(200).json(customResponse)
 
     }catch(err){
