edf38fba5aeab5a07defc2ae982181b9904f91b5
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-10-27 14:58:02 +0200

    Refactored Dates Generating feature to work on the fly 1.0

--- controller/clinickController.js
+++ controller/clinickController.js
@@ -1,12 +1,13 @@
 const Clinick = require('../models/clinickModule');
 const ResDates = require('../models/reservedDatesModel');
+const Appointment = require('../models/appointmentModel');
 const dateCalc = require('../util/dateCalculations');
 
 const moment = require('moment-timezone');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { default: mongoose } = require('mongoose');
-const { time } = require('console');
+// const { time } = require('console');
 const jwtSCRT = config.get('env_var.jwtScreteKey');
 
 //with doctorId as params
@@ -52,17 +53,17 @@ const getClinickById = async (req, res) => {
       return res.status(400).json({ message: 'Invalid clinic id' });
     }
 
-    const clinicBefore = await Clinick.findById(req.params.id);
+    // const clinicBefore = await Clinick.findById(req.params.id);
 
-    const resDate = await ResDates.find({ clinicId: req.params.id })
-      .select('-_id day time')
-      .exec();
+    // const resDate = await ResDates.find({ clinicId: req.params.id })
+    //   .select('-_id day time')
+    //   .exec();
 
-    console.log(resDate);
+    // console.log(resDate);
 
-    const clkReservedDates = await Clinick.findByIdAndUpdate(req.params.id, {
-      reservedDates: resDate,
-    }).exec();
+    // const clkReservedDates = await Clinick.findByIdAndUpdate(req.params.id, {
+    //   reservedDates: resDate,
+    // }).exec();
 
     const clinick = await Clinick.findById(req.params.id)
       .populate({
@@ -74,6 +75,12 @@ const getClinickById = async (req, res) => {
     if (!clinick) {
       return res.status(400).json({ message: 'Bad request' });
     }
+
+    clinick.reservedDates = await createReservedDatesRecord(
+      clinick._id,
+      clinick.openDates
+    );
+
     res.status(200).json(clinick);
   } catch (err) {
     console.log(err);
@@ -109,7 +116,7 @@ const addClinick = async (req, res) => {
       about,
     });
     await clinick.save();
-    await createReservedDatesRecord(clinick._id, clinick.openDates);
+    // await createReservedDatesRecord(clinick._id, clinick.openDates);
 
     res.status(200).json(clinick);
   } catch (err) {
@@ -182,10 +189,19 @@ const createReservedDatesRecord = async (clinicId, dates) => {
   console.log(days, time);
   //-------------------------------
 
-  let times = [];
-  for (let i = 0; i < time.length; i++) {
-    times.push(false);
-  }
+  const appointments = await Appointment.find({ clinicId }).exec();
+
+  let times = time.map((time) => {
+    let isReserved = false;
+    appointments.forEach((appointment) => {
+      appointment.time = appointment.appointmentDate.split(' ')[1];
+      if (appointment.time == time) {
+        isReserved = true;
+      }
+    });
+    return isReserved;
+  });
+
   let upcomingDays = [];
   days.forEach((day) => {
     upcomingDays.push(
@@ -195,67 +211,70 @@ const createReservedDatesRecord = async (clinicId, dates) => {
   //test-----------------
   console.log(upcomingDays);
   //----------------------------
+  let datesRecord = [];
   upcomingDays.forEach(async (days) => {
     days.forEach(async (day) => {
-      let datesRecord = new ResDates({
+      datesRecord.push({
         clinicId,
         day,
         time: times,
       });
-      await datesRecord.save();
+      // await datesRecord.save();
       console.log('Dates Saved successfully');
     });
   });
+
+  return datesRecord;
 };
 
 //This function will check for all dates that have been passed,
 //clean the db and update with the new date
-async function updateReservedDatesRecord() {
-  // const today = new Date().toISOString().substring(0, 10);
-  // const today = new Date().toISOString().substring(0, 10);
-  //tracing the week offset issue
-  const cairoTimezone = 'Africa/Cairo';
-  const today = moment().tz(cairoTimezone).format('YYYY-MM-DD');
-  console.log(today);
+// async function updateReservedDatesRecord() {
+//   // const today = new Date().toISOString().substring(0, 10);
+//   // const today = new Date().toISOString().substring(0, 10);
+//   //tracing the week offset issue
+//   const cairoTimezone = 'Africa/Cairo';
+//   const today = moment().tz(cairoTimezone).format('YYYY-MM-DD');
+//   console.log(today);
 
-  const passedDates = await ResDates.find({
-    day: { $lt: today },
-  });
+//   const passedDates = await ResDates.find({
+//     day: { $lt: today },
+//   });
 
-  // console.log(passedDates.day,'passedDates')
+//   // console.log(passedDates.day,'passedDates')
 
-  if (!passedDates.length == 0) {
-    passedDates.forEach(async (date) => {
-      console.log(date.day, 'date');
-      const day = date.day;
-      //get the time array in falsey values
-      const falsytime = date.time.map((time) => {
-        return false;
-      });
-      //get the day name
-      const dayName = dateCalc.getDayNameByDayHistory(day);
-      //get the day in the next month
-      const replaceDate = dateCalc.getUpcomingDatesForNUmberOfWeeks(
-        6,
-        dayName.toLowerCase()
-      )[5];
+//   if (!passedDates.length == 0) {
+//     passedDates.forEach(async (date) => {
+//       console.log(date.day, 'date');
+//       const day = date.day;
+//       //get the time array in falsey values
+//       const falsytime = date.time.map((time) => {
+//         return false;
+//       });
+//       //get the day name
+//       const dayName = dateCalc.getDayNameByDayHistory(day);
+//       //get the day in the next month
+//       const replaceDate = dateCalc.getUpcomingDatesForNUmberOfWeeks(
+//         6,
+//         dayName.toLowerCase()
+//       )[5];
 
-      await ResDates.create({
-        clinicId: date.clinicId,
-        day: replaceDate,
-        time: falsytime,
-      });
-      // console.log("Date created successfully", .day);
-    });
-    await ResDates.deleteMany({
-      day: { $lt: today },
-    });
-    // console.log("Date deleted successfully", oldDateRecord);
-  } else {
-    console.log('no dates is passed', passedDates);
-  }
+//       await ResDates.create({
+//         clinicId: date.clinicId,
+//         day: replaceDate,
+//         time: falsytime,
+//       });
+//       // console.log("Date created successfully", .day);
+//     });
+//     await ResDates.deleteMany({
+//       day: { $lt: today },
+//     });
+//     // console.log("Date deleted successfully", oldDateRecord);
+//   } else {
+//     console.log('no dates is passed', passedDates);
+//   }
 
-  setTimeout(updateReservedDatesRecord, 24 * 60 * 60 * 1000);
-}
+//   setTimeout(updateReservedDatesRecord, 24 * 60 * 60 * 1000);
+// }
 
-updateReservedDatesRecord();
+// updateReservedDatesRecord();
